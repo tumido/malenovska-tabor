56cbb421deaeaa2192499ad9d7e573ef4eb34530
vydrous <martin.andrysek13@gmail.com>
2022-05-03 10:57:55 +0200

    cast textu, jak to bude vypadat (#34)
    * Update Co.tsx
    
    * Update Co.tsx
    
    * Kecy o kostýmu
    
    * Update Co.tsx

--- containers/Co.tsx
+++ containers/Co.tsx
@@ -40,8 +40,33 @@ const Co = () => {
                 Mapy.cz
               </Button>
             </li>
-            <li>Termín: 20.8.2022 - 26.8.2022</li>
-            <li>Cena: 4000 Kč</li>
+            <li>Termín: 21.8.2022 - 27.8.2022</li>
+            <li>Cena: 4300 Kč</li>
+            <li>Letos oproti minulým ročníkům jsme si pro Vás připravili vícedenní LARP, na který od Vás potřebujeme, abyste si připrvili vlastní postavy.
+              S postavama Vám pomůže Vydra, můžete ho kontaktovat na FB, Discordu nebo na emailu vydra@malenovska.cz.
+              Abyste vědeli, co Vás čeká, zde je počáteční info, kde se bude LARP odehrávat</li>
+            <li>Děj našeho larpu se odehrává v malém, ale zdaleka ne bezvýznamném městečku. Nachází se na obchodní stezce, takže přitahuje spoustu bohatých, 
+            ale i divných lidí (a jiných bytostí). Někteří z vás zajisté slyšeli kolovat legendy o tom, že město vyhledává spousta mágů. Někteří z vás můžou ve měště již žít,
+            některé může lákat vidina bohatství, magicky nadané může lákat tajemno tohoto města. Ale taky můžete mít motivaci pro příchod úplně jiný.</li>
+            
+            <li>Protože bude tábor zaměřený především na hraní LARPu, je třeba s sebou mít kostým, který odpovídá vaší postavě. Také vás poprosíme
+              aby jste si pro letošní rok zajistili vlastní zbraně.</li>
+            
+            <li>Letošní tábor se bude konat v trochu jiném středisku než jste zvyklí. Základna se nachází u lesa u obce Křekov, v malebné přírodě v srdci Valašska.
+              Na základně je tekoucí pitná voda, sprcha, splachovací WC. 
+              Ubytování je ve stanech s podsadou a postelemi, nicméně je nutné si vzít dobrý spacák, na konci srpna už může být v noci zima. 
+              Doporučujeme si taky vzít karimatku pro případné spaní venku. 
+              Na základně je elektřina pouze v organizátorské chatě, ke které účastníci mají přístup výjimečně. Samozřejmě bude možnost nabít telefony na konci tábora,
+              abyste se mohli domluvit na odjezdu, ale z naší zkušenosti telefony na táboře jsou spíše na škodu.
+              V průběhu tábora bude možné kontaktovat organiátory, stejně tak účastníci budou mít možnost kontaktovat Vás rodiče přes organizátory.
+              Ale není poetičtější napsat dopis?</li>
+            <li>Co s sebou?</li>
+            <li>Kostým na Vaši postavu<br/>
+                Spacák<br/>
+                Karimatku<br/>
+                Baterku nebo jinou svítilnu (doporučujeme čelovku)<br/>
+                </li>
+            
           </OrderedList>
         </Grid>
         <Grid item xs={12} lg={6}>
